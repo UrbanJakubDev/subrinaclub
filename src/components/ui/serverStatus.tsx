c58fc58bd4641d13003a504ee630b0206d3f9e3d
UrbanJakubDev <jakub.73@hotmail.com>
2025-04-24 08:15:07 +0200

    refactor: Clean up formatting and improve code consistency in ServerStatus component
    - Standardize import statements and remove unnecessary line breaks for better readability.
    - Enhance the return structure of the ServerStatus component for improved clarity and maintainability.

--- src/components/ui/serverStatus.tsx
+++ src/components/ui/serverStatus.tsx
@@ -1,22 +1,23 @@
 'use client'
-import StatusChip from "../tables/ui/statusChip";
-import StatusIcon from "../tables/ui/statusIcon";
-import { useState } from "react";
+import StatusChip from '../tables/ui/statusChip'
+import StatusIcon from '../tables/ui/statusIcon'
+import { useState } from 'react'
 type ServerStatusProps = {
-   systemStatus: boolean;
-   lastUpdated: string;
-   id: number;
+    systemStatus: boolean
+    lastUpdated: string
+    id: number
 }
 
 export default function ServerStatus({ systemStatus, lastUpdated, id }: ServerStatusProps) {
-   return (
-      <div className="relative">
-         <div className="flex items-center  rounded-sm p-1 gap-1" >
-            <StatusIcon active={Boolean(systemStatus)} />
-            <span className="text-sm text-gray-500 text-nowrap">ID - {id}</span>
-            <span className="text-xs text-gray-500 text-right w-full">aktualizováno {lastUpdated}</span>
-         </div>
-    
-      </div>
-   );
+    return (
+        <div className="relative">
+            <div className="flex items-center  rounded-sm p-1 gap-1">
+                <StatusIcon active={Boolean(systemStatus)} />
+                <span className="text-sm text-gray-500 text-nowrap">ID - {id}</span>
+                <span className="text-xs text-gray-500 text-right w-full">
+                    aktualizováno {lastUpdated}
+                </span>
+            </div>
+        </div>
+    )
 }
